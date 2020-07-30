909f74a5c6f9b1e3063a596ea11a64c371493554
xianxiaoyin <525607894@qq.com>
2020-07-30 15:16:31 +0800

    添加测试脚本

--- spike.py
+++ spike.py
@@ -1,49 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time : 7/17/2020 1:28 PM
-# @Author : xianxiaoyin
+# @Author : XianXiaoYin
 
 from flask import Flask, request
 from flask_restful import Api
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from flask_redis import FlaskRedis
-
+from flask_cors import CORS
 
 redis_client = FlaskRedis()
 app = Flask(__name__)
 api = Api(app)
+CORS(app, supports_credentials=True)  # 支持跨域
 redis_client.init_app(app)
 
 
 class Spike(Resource):
-    def get(self):
+    @staticmethod
+    def get():
         data = {"msg": 'get get get'}
-        print(redis_client.get(111))
         return data, 200
 
-
-
-
-    def post(self):
+    @staticmethod
+    def post():
         """
         保存index  name如果超过到达10个就返回提示信息
         """
         name = request.form.get("name")
         total = redis_client.scard('spike')
-        if total > 10:
+        print(total)
+        if total > 10-1:
             return {"msg": '你来晚了，已经抢没了！'}, 302
         if name:
             redis_client.sadd('spike', name)
-
+        print(total)
         data = {'msg': "当前秒杀用户：{}".format(name)}
         return data, 201
 
 
-
-
-
 api.add_resource(Spike, '/')
 
- 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=80, debug=True)
