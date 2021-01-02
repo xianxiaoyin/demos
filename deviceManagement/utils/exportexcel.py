f10e5842daf1bb13166fb1ea264dcfffd4a1c031
xianyin <525607894@qq.com>
2021-01-02 19:06:06 +0800

    测试自动触发

--- deviceManagement/utils/exportexcel.py
+++ deviceManagement/utils/exportexcel.py
@@ -11,22 +11,21 @@ from devices.models import categorys, status, projects, functeams, locations
 from datetime import datetime
 
 
-
-def getIndex(modes_list, filterdata ):
-   return {v: k for k, v in dict(modes_list).items()}.get(filterdata)
-
+def getIndex(modes_list, filterdata):
+    return {v: k for k, v in dict(modes_list).items()}.get(filterdata)
 
 
 def readExcel(filename="1.xlsx"):
     data = []
     workbook = openpyxl.load_workbook(filename)
     ws = workbook.active
-    for row in ws.rows: 
+    for row in ws.rows:
         tmp_list = []
         for cel in row:
             tmp_list.append(cel.value)
         data.append(tmp_list)
     return data
+
 
 def saveData(filename):
     obj = []
@@ -40,14 +39,16 @@ def saveData(filename):
             # Devices.objects.bulk_create(obj)
             try:
                 Devices.objects.get_or_create(sn=line[1], bcode=line[2], category=getIndex(categorys, line[3]),
-                        status=getIndex(status, line[4]), project=getIndex(projects, line[5]),
-                        functeam=getIndex(functeams, line[6]),location=getIndex(locations, line[7]),
-                        po=line[8], rowner=line[9],wwid=line[10],
-                        comments=line[12],update_at=datetime.now(), create_at=datetime.now() )
-            except Exception as e  :
+                                              status=getIndex(status, line[4]), project=getIndex(projects, line[5]),
+                                              functeam=getIndex(functeams, line[6]),
+                                              location=getIndex(locations, line[7]),
+                                              po=line[8], rowner=line[9], wwid=line[10],
+                                              comments=line[12], update_at=datetime.now(), create_at=datetime.now())
+            except Exception as e:
                 print(getIndex(categorys, line[3]))
                 print(e)
                 print(line)
-            
+
+
 if __name__ == "__main__":
     saveData("1.xlsx ")
