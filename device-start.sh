d5ea2aee602b785918049e875103656ba264082a
xianxiaoyin <525607894@qq.com>
2021-01-14 15:34:47 +0800

    删除多余文件

--- device-start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --name dp -p  9000:9000 -d django-python3
-
--- device-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker ps -a  | grep dp
-
-if [ $? -eq 0 ]; then
-    docker stop dp && docker rm dp
-fi
-
-
