9fd0cd134edae70a015f8d9f16de2234ff4f8be7
xianyin <525607894@qq.com>
2021-01-17 12:07:14 +0800

    修改编排文件

--- compose/local/start.sh
+++ compose/local/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 python manage.py migrate
-python  manage.py   runserver 0:8080 --settings=deviceManagement.settings.dev
+python  manage.py   runserver 0:8000 --settings=deviceManagement.settings.dev
