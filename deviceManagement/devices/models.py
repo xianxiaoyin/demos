7ff5605b3c59c09aff8448c8ac09f6069e77a0fe
xianxiaoyin <525607894@qq.com>
2021-02-04 14:37:51 +0800

    修改数据库限制

--- deviceManagement/devices/models.py
+++ deviceManagement/devices/models.py
@@ -1,10 +1,10 @@
 # coding:utf-8
 '''
 Author: xianxiaoyin
-LastEditors: xianxiaoyin
+@,@LastEditors: ,: xianxiaoyin
 Descripttion: 数据库模型
 Date: 2020-12-19 12:30:13
-LastEditTime: 2021-02-01 13:58:59
+@,@LastEditTime: ,: 2021-02-04 14:29:18
 '''
 from datetime import datetime
 from django.db import models
@@ -19,7 +19,7 @@ class Status(models.Model):
     functeam = 4
     location = 5
     '''
-    name = models.CharField(verbose_name="status name", max_length=50, unique=True)
+    name = models.CharField(verbose_name="status name", max_length=200, unique=True)
     tag = models.CharField(verbose_name="tag", max_length=5)
     update_at = models.DateTimeField(verbose_name="update time", default=datetime.now)
     create_at = models.DateTimeField(verbose_name="create time", auto_now_add=True)
@@ -43,11 +43,11 @@ class Devices(models.Model):
      blank=True, null=True, related_name="functeam",default="")
     location = models.ForeignKey(Status, on_delete=SET_NULL,
      blank=True, null=True, related_name="location")
-    po = models.CharField(verbose_name="PO#", max_length=50, null=True, blank=True)
-    actual_user = models.CharField(verbose_name="actual_user", max_length=50, null=True, blank=True)
-    borrow_wwid = models.CharField(verbose_name="borrow_wwid", max_length=50, null=True, blank=True)
+    po = models.CharField(verbose_name="PO#", max_length=200, null=True, blank=True)
+    actual_user = models.CharField(verbose_name="actual_user", max_length=200, null=True, blank=True)
+    borrow_wwid = models.CharField(verbose_name="borrow_wwid", max_length=200, null=True, blank=True)
     po_requestor = models.CharField(verbose_name=" po_requestor",
-     max_length=50, null=True, blank=True)
+     max_length=200, null=True, blank=True)
     comments = models.TextField(verbose_name="comments", max_length=500, null=True, blank=True)
     update_at = models.DateTimeField(verbose_name="update time", auto_now=True)
     create_at = models.DateTimeField(verbose_name="create time", auto_now_add=True)
@@ -64,19 +64,19 @@ class HistoryUser(models.Model):
     """操作历史记录"""
     device_number = models.IntegerField(verbose_name="devices ID")
     bcode = models.CharField(verbose_name="change username",
-    max_length=50, null=True, blank=True)
+    max_length=200, null=True, blank=True)
     category = models.CharField(verbose_name="change category",
-     max_length=50, null=True, blank=True)
-    status = models.CharField(verbose_name="change status", max_length=50, null=True, blank=True)
-    project = models.CharField(verbose_name="change project", max_length=50, null=True, blank=True)
-    location = models.CharField(verbose_name="change location", max_length=50,
+     max_length=200, null=True, blank=True)
+    status = models.CharField(verbose_name="change status", max_length=200, null=True, blank=True)
+    project = models.CharField(verbose_name="change project", max_length=200, null=True, blank=True)
+    location = models.CharField(verbose_name="change location", max_length=200,
     null=True, blank=True)
     actual_user = models.CharField(verbose_name="change actual_user",
-    max_length=50, null=True, blank=True)
+    max_length=200, null=True, blank=True)
     borrow_wwid = models.CharField(verbose_name="change borrow_wwid",
-    max_length=50, null=True, blank=True)
+    max_length=200, null=True, blank=True)
     comments = models.CharField(verbose_name="change comments",
-    max_length=50, null=True, blank=True)
+    max_length=200, null=True, blank=True)
     create_at = models.DateTimeField(verbose_name="create time", auto_now_add=True)
     class Meta:
         db_table = "historyuser"
